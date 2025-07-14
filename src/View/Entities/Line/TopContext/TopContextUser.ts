d970fc666f996f70051249e86f273fc2d77f353e
Shan <shan@kaidhealth.com>
2025-07-13 21:55:56 -0400

    Reformat TopContextUser.ts

--- src/View/Entities/Line/TopContext/TopContextUser.ts
+++ src/View/Entities/Line/TopContext/TopContextUser.ts
@@ -1,20 +1,20 @@
 export abstract class TopContextUser {
-    readonly left!: number;
-    readonly width!: number;
-    layer: number = 0;
+  readonly left!: number;
+  readonly width!: number;
+  layer: number = 0;
 
-    abstract render(): SVGElement;
+  abstract render(): SVGElement;
 
-    abstract update(): void;
+  abstract update(): void;
 }
 
 export function overLaps(user1: TopContextUser, user2: TopContextUser): boolean {
-    if (user1.layer !== user2.layer) {
-        return false;
-    } else if (user1.left > user2.left) {
-        return overLaps(user2, user1);
-    } else if (user1.left + user1.width < user2.left) {
-        return false;
-    }
-    return true;
+  if (user1.layer !== user2.layer) {
+    return false;
+  } else if (user1.left > user2.left) {
+    return overLaps(user2, user1);
+  } else if (user1.left + user1.width < user2.left) {
+    return false;
+  }
+  return true;
 }

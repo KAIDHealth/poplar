885c055e8ca6a90a9c74fbacaf436156d7e9e948
Shan <shan@kaidhealth.com>
2025-07-16 14:21:31 -0400

    prepare for kaid-1.0.0

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,28 +1,29 @@
 const path = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
-    entry: './src/index.ts',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                loader: 'ts-loader',
-                exclude: [/node_modules/, /Demo/]
-            },
-        ]
-    },
-    resolve: {
-        extensions: [".ts", ".js"]
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js',
-        library: 'Poplar',
-        libraryTarget: "umd"
-    },
-    optimization: {
-        minimizer: [new UglifyJsPlugin()]
-    }
+  mode: 'production',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        exclude: [/node_modules/, /Demo/]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: 'Poplar',
+    libraryTarget: 'umd'
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()]
+  }
 };
